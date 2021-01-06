3d8e15699d66a3d9f6671a4f656be50a9db93b83
Ado Pabianko <adopabianko@gmail.com>
2021-01-06 16:57:48 +0700

    add placeholder login

--- cms/resources/views/auth/login.blade.php
+++ cms/resources/views/auth/login.blade.php
@@ -32,7 +32,7 @@
                 @csrf
                 
                 <div class="input-group mb-3">
-                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="Email" autofocus>
                     <div class="input-group-append">
                         <div class="input-group-text">
                             <span class="fas fa-envelope"></span>
@@ -45,7 +45,7 @@
                     @enderror
                 </div>
                 <div class="input-group mb-3">
-                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="Password">
                     <div class="input-group-append">
                         <div class="input-group-text">
                             <span class="fas fa-lock"></span>
